3e45f9ac282366666a8b002adbed82f6316812b1
WangXiao <2268971074@qq.com>
2020-11-25 18:31:56 +0800

    跳转功能

--- src/login.jsx
+++ src/login.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-
+import {createBrowserHistory} from 'history';
 export default class Login extends Component{
+
+    register(){
+        let history = createBrowserHistory();
+        history.push('/register');
+        history.go();
+    }
 
     render() {
         return(
             <div>
                 <h1>遇TA</h1>
                 <button>登录</button>
-                <button>注册</button>
+                <button onClick={this.register}>注册</button>
 
             </div>
         )
