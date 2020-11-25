ab0acbd277a8c6018d5cba895f5d803347b513af
WangXiao <2268971074@qq.com>
2020-11-25 21:37:10 +0800

    style completed

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -11,11 +11,13 @@ export default class Login extends Component {
 
   render() {
     return (
-      <div>
-        <h1>遇TA</h1>
-        <div>
-          <button>登录</button>
-          <button onClick={this.register}>注册</button>
+      <div className="container">
+        <div className="container-temp">
+          <h1 className="app-title">遇TA</h1>
+          <div className="btn-group">
+            <button className="btn none-default">登录</button>
+            <button className="btn none-default" onClick={this.register}>注册</button>
+          </div>
         </div>
       </div>
     )
--- src/register/register.jsx
+++ src/register/register.jsx
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import '../login/login.css';
 import './register.css';
 export default class Register extends Component {
 
   render() {
     return (
-      <div>
-        <h1>遇TA</h1>
-        <div>
-          <input placeholder="请输入手机号"></input>
-          <input placeholder="请输入密码"></input>
-          <input placeholder="请再次输入密码"></input>
+      <div className="container">
+        <div className="container-temp">
+          <h1 className="app-title app-title-2">遇TA</h1>
+          <div className="input-group">
+            <input className="reg-input none-default" placeholder="请输入手机号"></input>
+            <input className="reg-input none-default" placeholder="请输入密码"></input>
+            <input className="reg-input none-default" placeholder="请再次输入密码"></input>
+          </div>
+          <button className="reg-btn none-default">注册</button>
         </div>
-        <button>注册</button>
       </div>
     )
   }
