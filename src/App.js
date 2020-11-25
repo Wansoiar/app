b1f2f124cdea36fb35f7ff97b11770c0cd327a7c
WangXiao <2268971074@qq.com>
2020-11-25 17:28:20 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import {Router, Route, hashHistory} from 'react-router-dom';
+import Login from './login';
+import Register from './register';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+  render(){
+      return(
+        <Router history={hashHistory}>
+          <Route path="/" component={Login}/>
+          <Route path="/login" component={Login}/>
+          <Route path="/register" component={Register}/>
+        </Router>
+      )
+  }
 }
-
-export default App;
